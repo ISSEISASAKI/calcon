63ecd88e1f1052c3062695f7a7ad0cce9d83b31b
ISSEISASAKI <issei.601.inari@gmail.com>
2022-10-07 20:19:28 +0900

    Procfile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <!-- Fonts -->
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
   <div id="app">
